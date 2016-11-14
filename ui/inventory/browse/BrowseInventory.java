5c2e7e0546e2233f37526a681e6742a59f5fab29
Ken Prayogo <ken.prayogo@zomia.org>
2016-11-14 15:01:03 -0800

    BrowseInventory: Added a JPanel extension as an example

--- ui/inventory/browse/BrowseInventory.java
+++ ui/inventory/browse/BrowseInventory.java
@@ -6,8 +6,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BrowseInventory extends BaseFrame {
+	// Static values
 	private static final int scrWidth = 1020;
 	private static final int scrHeight = 720;
+	// Components
+	private JLabel title;
 
 	public BrowseInventory() {
 		super(scrWidth, scrHeight);
@@ -15,34 +18,22 @@ public class BrowseInventory extends BaseFrame {
 
 	@Override
 	protected void create() {
+		title = new JLabel("Inventory");
+		title.setHorizontalAlignment(JLabel.CENTER);
+		title.setVerticalAlignment(JLabel.NORTH);
 
 	}
 
 	@Override
 	protected void add() {
 		Container con = this.getContentPane();
-		GroupLayout layout = new GroupLayout(con);
+		// ToDo: GridLayout might not do it.. Controls are taking up the whole height
+		GridLayout layout = new GridLayout(0,3);
 		con.setLayout(layout);
 
-		layout.setAutoCreateGaps(true);
-		layout.setAutoCreateContainerGaps(true);
-
-//		layout.setHorizontalGroup(
-//				layout.createSequentialGroup()
-//						.addComponent(c1)
-//						.addComponent(c2)
-//						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-//								          .addComponent(c3)
-//								          .addComponent(c4))
-//		);
-//		layout.setVerticalGroup(
-//				layout.createSequentialGroup()
-//						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-//								          .addComponent(c1)
-//								          .addComponent(c2)
-//								          .addComponent(c3))
-//						.addComponent(c4)
-//		);
+		TestPanel p = new TestPanel();
+		con.add(p);
+		con.add(title);
 	}
 
 	@Override
@@ -51,6 +42,27 @@ public class BrowseInventory extends BaseFrame {
 	}
 
 	public static void main(String args[]) {
-		new BrowseInventory();
+		JFrame f = new BrowseInventory();
+	}
+}
+
+/**
+ * Example custom JPanel to group controls into one
+ * Uses the BoxLayout, which places controls vertically
+ * (Still some issues with the combo box width)
+ */
+class TestPanel extends JPanel {
+
+	public TestPanel() {
+		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+		JComboBox box = new JComboBox();
+		JComboBox box2 = new JComboBox();
+		box.addItem("First box");
+		box2.addItem("Second box");
+
+		add(new JLabel("I am inside a panel."));
+		add(box);
+		add(box2);
 	}
 }
