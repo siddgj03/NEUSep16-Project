ca53a07d3485769753f0d8ea3e688e14d4ff6021
CARLTON XIAOTONG CHEN <mrchenxt@foxmail.com>
2016-11-19 23:16:28 -0800

    Create carMakeList.java

---
+++ SepcialManagement_Group4/carMakeList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class carMakeList {
+	
+	public static ArrayList getCarMakeList(){
+		ArrayList carMakeList=new ArrayList();
+		
+		carMakeList.add("All"); carMakeList.add("Acura"); carMakeList.add("Alfa Romeo"); carMakeList.add("AM General"); 
+		carMakeList.add("Aston Martin"); carMakeList.add("Audi"); carMakeList.add("Bentley"); carMakeList.add("BMW"); 
+		carMakeList.add("Bugatti"); carMakeList.add("Buick"); carMakeList.add("Cadillac"); carMakeList.add("Chevrolet"); 
+		carMakeList.add("Chrysler"); carMakeList.add("Daewoo"); carMakeList.add("Dodge"); carMakeList.add("Eagle"); 
+		carMakeList.add("Ferrari"); carMakeList.add("FIAT"); carMakeList.add("Fisker"); carMakeList.add("Ford"); 
+		carMakeList.add("Genesis"); carMakeList.add("Geo"); carMakeList.add("GMC"); carMakeList.add("Honda"); 
+		carMakeList.add("HUMMER"); carMakeList.add("Hyundai"); carMakeList.add("Infiniti"); carMakeList.add("Isuzu"); 
+		carMakeList.add("Jaguar"); carMakeList.add("Jeep"); carMakeList.add("Kia"); carMakeList.add("Lamborghini"); 
+		carMakeList.add("Land Rover"); carMakeList.add("Lexus"); carMakeList.add("Lincoln"); carMakeList.add("Lotus"); 
+		carMakeList.add("Maserati"); carMakeList.add("Maybach"); carMakeList.add("Mazda"); carMakeList.add("McLaren"); 
+		carMakeList.add("Mercedes-Benz"); carMakeList.add("Mercury"); carMakeList.add("MINI"); carMakeList.add("Mitsubishi"); 
+		carMakeList.add("Nissan"); carMakeList.add("Oldsmobile"); carMakeList.add("Panoz"); carMakeList.add("Plymouth"); 
+		carMakeList.add("Pontiac"); carMakeList.add("Porsche"); carMakeList.add("Ram"); carMakeList.add("Rolls-Royce"); 
+		carMakeList.add("Saab"); carMakeList.add("Saturn"); carMakeList.add("Scion"); carMakeList.add("Smart"); 
+		carMakeList.add("Spyker"); carMakeList.add("Subaru"); carMakeList.add("Suzuki"); carMakeList.add("Tesla"); 
+		carMakeList.add("Toyota"); carMakeList.add("Volkswagen"); carMakeList.add("Volvo");
+    
+		return carMakeList;
+		
+	}
+}
