ccf5fcbb528883f8fc822d8adbbb09d7d44425a3
CARLTON XIAOTONG CHEN <mrchenxt@foxmail.com>
2016-11-19 23:17:35 -0800

    Create carYearList.java

---
+++ SepcialManagement_Group4/carYearList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class carYearList {
+	
+	public static ArrayList getCarYearList(){
+		ArrayList caeYearList=new ArrayList();
+		
+		caeYearList.add("2017"); caeYearList.add("2016"); caeYearList.add("2015"); caeYearList.add("2014"); 
+		caeYearList.add("2013"); caeYearList.add("2012"); caeYearList.add("2011"); caeYearList.add("2010"); 
+		caeYearList.add("2009"); caeYearList.add("2008"); caeYearList.add("2007"); caeYearList.add("2006"); 
+		caeYearList.add("2005"); caeYearList.add("2004"); caeYearList.add("2003"); caeYearList.add("2002"); 
+		caeYearList.add("2001"); caeYearList.add("2000"); caeYearList.add("1999"); caeYearList.add("1998"); 
+		
+		return caeYearList;
+	}
+}
