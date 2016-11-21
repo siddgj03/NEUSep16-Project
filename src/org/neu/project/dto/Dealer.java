8004bcda1bad91ca2007914d08d7ab85cf9c4a09
ng887 <nehagupta887@gmail.com>
2016-11-21 10:41:05 -0800

    Update Dealer and Vehicle Class

--- src/org/neu/project/dto/Dealer.java
+++ src/org/neu/project/dto/Dealer.java
@@ -1,13 +1,43 @@
 package org.neu.project.dto;
 
 public class Dealer {
-	private String name;
+    
+    private String id;
+    private String name;
+    private String url;
+    
+    public Dealer() {
+        
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Dealer(String id, String name, String url) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.url = url;
+    }
+    
+    public String getId() {
+        return id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
