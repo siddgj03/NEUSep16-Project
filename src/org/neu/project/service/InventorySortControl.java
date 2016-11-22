e97fda4d195cb8a5de2b700d397a244ac75f8d21
Xiaoyue Li <lixiaoyue.314@gmail.com>
2016-11-22 13:48:51 -0800

    Update InventorySortControl Class and InventorySearchControl Class

---
+++ src/org/neu/project/service/InventorySortControl.java
@@ -0,0 +1,46 @@
+package src.org.neu.project.service;
+
+public class InventorySortControl {
+    
+    public List<Vehicle> sortByMake(Colloction<Vehicle> vehicle, boolean isDesc) {
+        
+        
+        return null;
+    }
+    
+    public List<Vehicle> sortByType(Colloction<Vehicle> vehicle, boolean isDesc) {
+        
+        return null;
+    }
+
+    public List<Vehicle> sortByCategory(Colloction<Vehicle> vehicle, boolean isDesc) {
+        
+        return null;
+    }
+    
+    public List<Vehicle> sortByCarId(Colloction<Vehicle> vehicle, boolean isDesc) {
+        
+        return null;
+    }
+    
+    public List<Vehicle> sortByYear(Colloction<Vehicle> vehicle, boolean isDesc) {
+        
+        return null;
+    }
+    
+    public List<Vehicle> sortByModel(Colloction<Vehicle> vehicle, boolean isDesc) {
+        
+        return null;
+    }
+    
+    public List<Vehicle> sortByTrim(Colloction<Vehicle> vehicle, boolean isDesc) {
+        
+        return null;
+    }
+    
+    public List<Vehicle> sortByPrice(Colloction<Vehicle> vehicle, boolean isDesc) {
+        
+        return null;
+    }
+
+}
