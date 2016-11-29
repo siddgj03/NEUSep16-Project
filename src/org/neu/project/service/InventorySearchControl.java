c814cebd19d3b02bc7dcb7fbb0f8a38244899a05
siddgj03 <siddhant.gujar03@gmail.com>
2016-11-28 17:43:59 -0800

    Update InventorySearchControl.java

--- src/org/neu/project/service/InventorySearchControl.java
+++ src/org/neu/project/service/InventorySearchControl.java
@@ -1,48 +1,94 @@
 package src.org.neu.project.service;
 
-import java.util.List;
+import java.util.*;
+
 
 public class InventorySearchControl {
-
-	
-	public Colloction<Vehicle> filterMake(Colloction<Vehicle> vehicle, String make) {
+	public List<vehicle> filterMake(List<vehicle> vehicles, String make) {
 	    
-	    return null;
+		List<vehicle> result = new ArrayList<vehicle>();
+	    for(vehicle v : vehicles){
+	    	if(v.getMake().equals(make))
+	    		result.add(v);
+	    }
+	    
+	    return result;
 	}
 	
-    public Colloction<Vehicle> filterType(Colloction<Vehicle> vehicle, String type) {
-        
-        return null;
+    public List<vehicle> filterType(List<vehicle> vehicles, String type) {
+
+		List<vehicle> result = new ArrayList<vehicle>();
+	    for(vehicle v : vehicles){
+	    	if(v.getType().equals(type))
+	    		result.add(v);
+	    }
+	    
+	    return result;
     }
 
-    public Colloction<Vehicle> filterCategory(Colloction<Vehicle> vehicle, String category) {
-        
-        return null;
+    public List<vehicle> filterCategory(List<vehicle> vehicles, String category) {
+
+		List<vehicle> result = new ArrayList<vehicle>();
+	    for(vehicle v : vehicles){
+	    	if(v.getCategory().equals(category))
+	    		result.add(v);
+	    }
+	    
+	    return result;
     }
     
-    public Colloction<Vehicle> filterCarId(Colloction<Vehicle> vehicle, String carId) {
-        
-        return null;
+    public List<vehicle> filterCarId(List<vehicle> vehicles, String carId) {
+
+		List<vehicle> result = new ArrayList<vehicle>();
+	    for(vehicle v : vehicles){
+	    	if(v.getId().equals(carId))
+	    		result.add(v);
+	    }
+	    
+	    return result;
     }
     
-    public Colloction<Vehicle> filterYear(Colloction<Vehicle> vehicle, int year) {
-        
-        return null;
+    public List<vehicle> filterYear(List<vehicle> vehicles, int year) {
+
+		List<vehicle> result = new ArrayList<vehicle>();
+	    for(vehicle v : vehicles){
+	    	if(v.getYear()==year)
+	    		result.add(v);
+	    }
+	    
+	    return result;
     }
     
-    public Colloction<Vehicle> filterModel(Colloction<Vehicle> vehicle, String model) {
-        
-        return null;
+    public List<vehicle> filterModel(List<vehicle> vehicles, String model) {
+
+		List<vehicle> result = new ArrayList<vehicle>();
+	    for(vehicle v : vehicles){
+	    	if(v.getModel().equals(model))
+	    		result.add(v);
+	    }
+	    
+	    return result;
     }
     
-    public Colloction<Vehicle> filterTrim(Colloction<Vehicle> vehicle, String trim) {
-        
-        return null;
+    public List<vehicle> filterTrim(List<vehicle> vehicles, String trim) {
+
+		List<vehicle> result = new ArrayList<vehicle>();
+	    for(vehicle v : vehicles){
+	    	if(v.getTrim().equals(trim))
+	    		result.add(v);
+	    }
+	    
+	    return result;
     }
 	
-    public Colloction<Vehicle> filterPrice(Colloction<Vehicle> vehicle, int price) {
-        
-        return null;
-    }
+    public List<vehicle> filterPrice(List<vehicle> vehicles, int price) {
 
+		List<vehicle> result = new ArrayList<vehicle>();
+	    for(vehicle v : vehicles){
+	    	if(v.getPrice()<=price)
+	    		result.add(v);
+	    }
+	    
+	    return result;
+    }
 }
