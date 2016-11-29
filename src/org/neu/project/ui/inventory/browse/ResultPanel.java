21556f39fee5c0f0c68223d949d971031e6b0067
Sida Niu <niu.sid@husky.neu.edu>
2016-11-28 16:53:24 -0800

    result panel layout changes

--- src/org/neu/project/ui/inventory/browse/ResultPanel.java
+++ src/org/neu/project/ui/inventory/browse/ResultPanel.java
@@ -8,11 +8,13 @@ import java.awt.event.ActionListener;
 import java.util.Collection;
 
 public class ResultPanel extends JPanel {
+	
+//	private Image image;
 
 	private JPanel carResultCombo;
 
 	private JLabel id;
-	private JLabel webId;
+//	private JLabel webId;
 	private JLabel category;
 	private JLabel year;
 	private JLabel make;
@@ -20,13 +22,13 @@ public class ResultPanel extends JPanel {
 	private JLabel trim;
 	private JLabel type;
 	private JLabel price;
-
+	private JLabel special;
 
 	public ResultPanel(Collection<Vehicle> vehicles) {
 
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-		ButtonGroup selects  = new ButtonGroup();
+		ButtonGroup selects = new ButtonGroup();
 
 		for (Vehicle object : vehicles) {
 
@@ -43,14 +45,14 @@ public class ResultPanel extends JPanel {
 		carResultCombo = new JPanel();
 		GroupLayout layout = new GroupLayout(carResultCombo);
 		carResultCombo.setLayout(layout);
-		carResultCombo.setBorder(BorderFactory.createTitledBorder( "details "));
+		carResultCombo.setBorder(BorderFactory.createTitledBorder("details "));
 
 		JRadioButton select = new JRadioButton("Select ");
 		ClickMeListener cml = new ClickMeListener(vehicle.getId());
 		select.addActionListener(cml);
 
 		id = new JLabel("ID:   " + vehicle.getId());
-//		webId = new JLabel("webId:   " + vehicle.getWebId());
+		// webId = new JLabel("webId: " + vehicle.getWebId());
 		category = new JLabel("Category:   " + vehicle.getCategory());
 		year = new JLabel("Year:   " + vehicle.getYear());
 		make = new JLabel("Make:   " + vehicle.getMake());
@@ -58,30 +60,31 @@ public class ResultPanel extends JPanel {
 		trim = new JLabel("Trim:   " + vehicle.getTrim());
 		type = new JLabel("Type:   " + vehicle.getType());
 		price = new JLabel("Price:   " + vehicle.getPrice());
+		special = new JLabel("Special:   ");
 
 		GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
-		hGroup.addGap(10);
-		hGroup.addGroup(layout.createParallelGroup().addComponent(type).addComponent(make).addComponent(model));
-		hGroup.addGap(10);
-		hGroup.addGroup(layout.createParallelGroup().addComponent(id).addComponent(category)
-				.addComponent(year).addComponent(trim));
-		hGroup.addGap(10);
-		hGroup.addGroup(layout.createParallelGroup().addComponent(price).addComponent(select));
-		hGroup.addGap(10);
+		hGroup.addGap(20);
+		hGroup.addGroup(layout.createParallelGroup().addComponent(category).addComponent(make));
+		hGroup.addGap(20);
+		hGroup.addGroup(layout.createParallelGroup().addComponent(id).addComponent(type).addComponent(year)
+				.addComponent(model).addComponent(trim));
+		hGroup.addGap(20);
+		hGroup.addGroup(layout.createParallelGroup().addComponent(price).addComponent(special).addComponent(select));
+		hGroup.addGap(20);
 		layout.setHorizontalGroup(hGroup);
-		
+
 		GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
-		vGroup.addGap(10);
-		vGroup.addGroup(layout.createParallelGroup().addComponent(id));
-		vGroup.addGap(10);
-		vGroup.addGroup(layout.createParallelGroup().addComponent(type));
-		vGroup.addGap(10);
-		vGroup.addGroup(layout.createParallelGroup().addComponent(make).addComponent(category).addComponent(price));
-		vGroup.addGap(10);
-		vGroup.addGroup(layout.createParallelGroup().addComponent(model).addComponent(year));
-		vGroup.addGap(10);
+		vGroup.addGap(20);
+		vGroup.addGroup(layout.createParallelGroup().addComponent(category).addComponent(id));
+		vGroup.addGap(20);
+		vGroup.addGroup(layout.createParallelGroup().addComponent(type).addComponent(price));
+		vGroup.addGap(20);
+		vGroup.addGroup(layout.createParallelGroup().addComponent(year).addComponent(special));
+		vGroup.addGap(20);
+		vGroup.addGroup(layout.createParallelGroup().addComponent(make).addComponent(model));
+		vGroup.addGap(20);
 		vGroup.addGroup(layout.createParallelGroup().addComponent(trim).addComponent(select));
-		vGroup.addGap(10);
+		vGroup.addGap(20);
 		layout.setVerticalGroup(vGroup);
 
 		return carResultCombo;
@@ -105,7 +108,6 @@ public class ResultPanel extends JPanel {
 }
 
 class ClickMeListener implements ActionListener {
-	int cnt = 0;
 	private String vehicleId;
 
 	public ClickMeListener(String vehicleId) {
@@ -114,11 +116,10 @@ class ClickMeListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (cnt % 2 == 0) {
-			BrowseInventory.setSelectedVehicleId(vehicleId);
-			System.out.println("Car selected: " + vehicleId);
-		}
-		cnt++;
+
+		BrowseInventory.setSelectedVehicleId(vehicleId);
+		System.out.println("Car selected: " + vehicleId);
 
 	}
+
 }
