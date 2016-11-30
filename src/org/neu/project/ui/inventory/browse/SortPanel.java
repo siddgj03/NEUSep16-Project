f160c4b1cd7bd169109d6ebd2e20e4ee34ddd61b
Ying Liao <liaoying.shufe@gmail.com>
2016-11-29 23:42:10 -0800

    Updated SortPanel by adding comments

--- src/org/neu/project/ui/inventory/browse/SortPanel.java
+++ src/org/neu/project/ui/inventory/browse/SortPanel.java
@@ -16,9 +16,11 @@ import org.neu.project.dto.Vehicle;
 
 class SortPanel extends JPanel
 {
+    // Components for sort UI
     private JComboBox<SortItems> sortList;
 	private JLabel sortLabel;
 
+	// Constructor with the consolidated frame for getting/setting inventory purpose
 	public SortPanel(BrowseInventory frame)
 	{
 		this.setLayout(new FlowLayout());
@@ -26,31 +28,30 @@ class SortPanel extends JPanel
 		sortList.addItem(new SortItems("Select Sort By"));
 		sortList.addItem(new SortItems("Price: High To Low", "Price", "DESC"));
 		sortList.addItem(new SortItems("Price: Low To High", "Price", "ASC"));
-		sortList.addItem(new SortItems("Year: High To Low", "Price", "DESC"));
-		sortList.addItem(new SortItems("Year: Low To High", "Price", "ASC"));
-		sortList.addItem(new SortItems("Model: A - Z", "Price", "ASC"));
-		sortList.addItem(new SortItems("Model: Z - A", "Price", "DESC"));
-		sortList.addItem(new SortItems("Specials", "Price", "DES"));
+		sortList.addItem(new SortItems("Year: High To Low", "Year", "DESC"));
+		sortList.addItem(new SortItems("Year: Low To High", "Year", "ASC"));
+		sortList.addItem(new SortItems("Model: A - Z", "Model", "ASC"));
+		sortList.addItem(new SortItems("Model: Z - A", "Model", "DESC"));
+		sortList.addItem(new SortItems("Specials", "Specials", "DES"));
 		sortLabel = new JLabel("Sort By:");
 		add(sortLabel);
 		add(sortList);
 		sortList.addItemListener(new ItemChangeListener(frame));
 
-		/**
-		 * Can not implement due to data limitation
-		 sortList.addItem(new SortItems("Mileage: High To Low"));
-		 sortList.addItem(new SortItems("Mileage: Low To High"));
-		 sortList.addItem(new SortItems("HWY MPG: High To Low"));
-		 sortList.addItem(new SortItems("HWY MPG: Low To High"));
-		 sortList.addItem(new SortItems("Exterior Color: A - Z"));
-		 sortList.addItem(new SortItems("Exterior Color: Z - A"));
-		 sortList.addItem(new SortItems("Certified"));
-		 *
-		 */
+//		 Can not implement due to data limitation
+//		 sortList.addItem(new SortItems("Mileage: High To Low"));
+//		 sortList.addItem(new SortItems("Mileage: Low To High"));
+//		 sortList.addItem(new SortItems("HWY MPG: High To Low"));
+//		 sortList.addItem(new SortItems("HWY MPG: Low To High"));
+//		 sortList.addItem(new SortItems("Exterior Color: A - Z"));
+//		 sortList.addItem(new SortItems("Exterior Color: Z - A"));
+//		 sortList.addItem(new SortItems("Certified"));
+
 	}
 
 }
 
+//SortItems class to specify all the sort criterion
 class SortItems
 {
 	private String sortName;
@@ -65,17 +66,27 @@ class SortItems
 		this.sortName = sortName;
 	}
 
+    /**
+     * Main constructor
+     * @param sortName - content displayed in the sort ComboBox
+     * @param sortKey - keyword for sort criterion - e.g. Price
+     * @param sortOrder - desc or asc
+     */
 	public SortItems(String sortName, String sortKey, String sortOrder)
 	{
 		this.sortName = sortName;
 		this.sortKey = sortKey;
 		this.sortOrder = sortOrder;
+        sortControl = new InventorySortControl();
 	}
 
+    /** Consolidate the sort function
+     * @param inv - inventory passed from search result
+     * @return sorted inventory list
+     */
 	public Collection<Vehicle> doSort(Collection<Vehicle> inv)
 	{
-		sortControl = new InventorySortControl();
-		return sortControl.sort(inv, this.sortKey, this.sortOrder);
+		return this.sortControl.sort(inv, this.sortKey, this.sortOrder);
 	}
 
 	public String toString()
@@ -83,6 +94,7 @@ class SortItems
 		return this.sortName;
 	}
 
+	// Excluded the head item - "Select Sort By"
 	public boolean isSortable()
 	{
 		return this.sortKey != null && this.sortOrder != null;
@@ -90,6 +102,7 @@ class SortItems
 
 }
 
+// SortItems action listener to trigger related sort operation
 class ItemChangeListener implements ItemListener
 {
 	private BrowseInventory frame;
