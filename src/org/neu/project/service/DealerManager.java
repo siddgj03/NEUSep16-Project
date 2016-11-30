9fc6b653d3aef5771e453397de565bab72781984
ng887 <nehagupta887@gmail.com>
2016-11-30 13:18:21 -0800

    update

--- src/org/neu/project/service/DealerManager.java
+++ src/org/neu/project/service/DealerManager.java
@@ -1,10 +1,9 @@
 package org.neu.project.service;
 
-import java.util.List;
-
-import org.neu.project.dto.Dealer;
+import java.util.Set;
 
 public interface DealerManager {
+	
 	public Set<String> getDealersName();
 
 	public String getDealerIDbyName(String name);
