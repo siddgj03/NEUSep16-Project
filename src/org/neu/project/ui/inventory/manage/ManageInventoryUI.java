c65d06a80a787a27b5753f84a18fda3edbc9a0bd
Jim-Pai <a616132003@gmail.com>
2016-12-02 14:41:18 -0800

    Modify ManageInventoryUI.

--- src/org/neu/project/ui/inventory/manage/ManageInventoryUI.java
+++ src/org/neu/project/ui/inventory/manage/ManageInventoryUI.java
@@ -266,7 +266,7 @@ public class ManageInventoryUI extends BaseFrame{
 //    	
 //    	new ManageInventoryUI(vehicle, false);
 //    } 
- 
+
 // Scenario one : Add a new Vehicle
 //    public static void main(String[] args) {
 //	  	Vehicle vehicle2 = new Vehicle("gmps-bertogden-cch");
