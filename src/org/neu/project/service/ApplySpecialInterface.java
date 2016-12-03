371898c3cb8f13d6a08d08c528f1aa9766e51fb3
Zeyuan Li <li.ze@husky.neu.edu>
2016-12-02 16:41:05 -0800

    Update ApplySpecialInterface.java

--- src/org/neu/project/service/ApplySpecialInterface.java
+++ src/org/neu/project/service/ApplySpecialInterface.java
@@ -1,7 +1,12 @@
 package org.neu.project.service;
+
 import java.text.ParseException;
 import java.util.TreeSet;
+
+import org.neu.project.dto.CarSpecificSpecialList;
+
 interface ApplySpecialInterface {
 	double getLowestPrice() throws ParseException;
+
 	TreeSet<CarSpecificSpecialList> getSplTree();
 }
