469e764221eb92ce577b90eb271f00cfd3f17e91
Zeyuan Li <li.ze@husky.neu.edu>
2016-12-02 16:41:27 -0800

    Update CompareByExpiry.java

--- src/org/neu/project/service/CompareByExpiry.java
+++ src/org/neu/project/service/CompareByExpiry.java
@@ -5,20 +5,21 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Comparator;
 
-public class CompareByExpiry implements Comparator<CarSpecificSpecialList>{
+import org.neu.project.dto.CarSpecificSpecialList;
+
+public class CompareByExpiry implements Comparator<CarSpecificSpecialList> {
 	@Override
-	public int compare(CarSpecificSpecialList o1, CarSpecificSpecialList o2) 
-	{   int i=-1;
+	public int compare(CarSpecificSpecialList o1, CarSpecificSpecialList o2) {
+		int i = -1;
 		DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		try {
-			if((df.parse(df.format(o1.expiry))).after(df.parse(df.format(o2.expiry))))
-				i=1;
-				else
-					i=-1;
-		}
-		 catch (ParseException e) {
+			if ((df.parse(df.format(o1.getExpiry()))).after(df.parse(df.format(o2.getExpiry()))))
+				i = 1;
+			else
+				i = -1;
+		} catch (ParseException e) {
 			e.printStackTrace();
-		}	
-return i;
+		}
+		return i;
+	}
 }
-} 
