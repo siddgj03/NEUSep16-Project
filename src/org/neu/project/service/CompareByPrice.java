0731adcd3ff22a6c4af8dbb33df187ebece77be2
Zeyuan Li <li.ze@husky.neu.edu>
2016-12-02 16:47:11 -0800

    Update CompareByPrice.java

--- src/org/neu/project/service/CompareByPrice.java
+++ src/org/neu/project/service/CompareByPrice.java
@@ -1,11 +1,13 @@
 package org.neu.project.service;
 
 import java.util.Comparator;
+
+import org.neu.project.dto.CarSpecificSpecialList;
 public class CompareByPrice implements Comparator<CarSpecificSpecialList>{
 	@Override
 	public int compare(CarSpecificSpecialList o1, CarSpecificSpecialList o2) 
 	{
-		if(o1.specialledPrice>=o2.specialledPrice)
+		if(o1.getSpecialledPrice()>=o2.getSpecialledPrice())
 			    return 1 ;
 			else
 				return -1;	
